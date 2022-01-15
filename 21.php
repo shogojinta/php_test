032721fb93f93f1c3ea16465cffc25feb3cacb66
jin1125 <shogo.jinta@gmail.com>
2022-01-15 19:54:24 +0900

    21

--- 21.php
+++ 21.php
@@ -6,4 +6,28 @@ function test(){
 
 test();
 
+$comment = 'comme!!';
+
+function getComment($string){
+  echo $string;
+}
+
+getComment($comment);
+
+function getNumberOfComment(){
+  return 5;
+}
+
+var_dump(getNumberOfComment());
+echo getNumberOfComment();
+
+function sumPrice($int1,$int2){
+  $int3 = $int1 + $int2;
+  return $int3;
+} 
+
+$total = sumPrice(3,5);
+
+echo $total;
+
 ?>
